233b8a846fc914d28989db60bcdf874d007f85ee
drzenit <starikovandru@gmail.com>
2020-05-04 00:15:27 +0300

    add graph. done A of thirdTask

--- labOne/kNN.py
+++ labOne/kNN.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
 
 
 def thirdTask(dataPath: str):
@@ -14,24 +15,37 @@ def thirdTask(dataPath: str):
     # Разделение на признаки и метки
     feature = dataset.iloc[:, :-1]
     label = dataset['Type']
-    print(feature)
-    print(label)
 
     # Деление данных на обучающие и тестовые
     trainSize = 0.7
     testSize = 1 - trainSize
     feature_train, feature_test, label_train, label_test = train_test_split(feature, label, train_size=trainSize, test_size=testSize)
 
-    # Создание и обучение экземпляра классификатора
-    knnCl = KNeighborsClassifier(n_neighbors=5)
-    knnCl.fit(feature_train, label_train)
+    # Вариация K
+    kList = list()
+    errList = list()
+    for k in range(1, 150):
+        # Создание и обучение экземпляра классификатора
+        knnCl = KNeighborsClassifier(n_neighbors=k)
+        knnCl.fit(feature_train, label_train)
 
-    # Тестирование классификатора
-    knnResult = knnCl.predict(feature_test)
+        # Тестирование классификатора
+        knnResult = knnCl.predict(feature_test)
 
-    # Оценка точности классификатора
-    knnAccuracy = accuracy_score(knnResult, label_test)
-    print(knnAccuracy)
+        # Оценка точности классификатора
+        knnAccuracy = accuracy_score(knnResult, label_test)
+        knnError = 1 - knnAccuracy
+        print(knnError)
+
+        # Сбор параметров для графика
+        kList.append(k)
+        errList.append(knnError)
+
+    # Построение графика зависимости ошибки классификации от количества ближайших соседей
+    plt.plot(kList, errList)
+    plt.xlabel("K")
+    plt.ylabel("Error")
+    plt.show()
 
 
 thirdTask("data\glass.csv")
